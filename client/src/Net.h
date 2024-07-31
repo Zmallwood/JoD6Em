a4397f2298d1417d96b52f5dccb1988d650510b3
Zmallwood <zmallwood@proton.me>
2024-07-31 17:47:57 +0200

    Format

--- client/src/Net.h
+++ client/src/Net.h
@@ -4,46 +4,32 @@
 
 #include "MessageCodes.h"
 
-namespace jod
-{
-    /**
-     * @brief Connects to to web socket server and enables sending messages over the connection
-     * after it has opened.
-     *
-     */
-    class WebSocketServerConnection
-    {
-      public:
-        /**
-         * @brief Open the connection to the web socket server.
-         *
-         */
+namespace jod {
+    /////////////////////////////////////////////////
+    /// \brief Connects to to web socket server and enables sending messages over the connection
+    ///        after it has opened.
+    /////////////////////////////////////////////////
+    class WebSocketServerConnection {
+    public:
+        /////////////////////////////////////////////////
+        /// \brief Open the connection to the web socket server.
+        /////////////////////////////////////////////////
         void Connect();
-
-        /**
-         * @brief Send message to the server.
-         *
-         * @param messageType Message type to be sent.
-         */
+        
+        /////////////////////////////////////////////////
+        /// \brief Send message to the server.
+        /// 
+        /// \param messageType Message type to be sent.
+        /////////////////////////////////////////////////
         void SendMessage(int messageType);
-
-        /**
-         * @brief Web socket event object related to the opened connection.
-         *
-         */
+        
+        /////////////////////////////////////////////////
+        /// \brief Web socket event object related to the opened connection.
+        /////////////////////////////////////////////////
         std::shared_ptr<const EmscriptenWebSocketOpenEvent> m_webSocketEvent;
-
-      private:
-        /**
-         * @brief Host name of the server to connect to.
-         *
-         */
-        const std::string k_host = "localhost";
-
-        /**
-         * @brief Port number to open the connection at.
-         *
-         */
-        const int k_port = 8765;
+        
+    private:
+        const std::string k_host = "localhost"; ///< Host name of the server to connect to.
+        const int k_port = 8765; ///< Port number to open the connection at.
     };
 }
