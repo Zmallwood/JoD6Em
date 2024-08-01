4f3867c3bcbbafdac94d931916d0a73c92762480
Zmallwood <zmallwood@proton.me>
2024-08-01 15:31:43 +0200

    Save

--- client/src/image_bank.h
+++ client/src/image_bank.h
@@ -3,40 +3,34 @@
 #pragma once
 
 namespace jod {
-    /**
-     * Loads and provides image resources.
+    /** \brief Loads and provides image resources.
      */
     class image_bank {
     public:
-        /**
-         * Construct a new Image Bank object.
+        /** \brief Construct a new Image Bank object.
          */
         image_bank();
         
-        /**
-         * Destroy the image bank object.
+        /** \brief Destroy the image bank object.
          */
         ~image_bank();
         
-        /**
-         * Get the Image object.
-         * @param imageNameName of image, corresponding to filename wihtout extension.
-         * @return GLuintGLuint Resource ID of obtained image object.
+        /** Get the Image object.
+         *  \param imageNameName of image, corresponding to filename wihtout extension.
+         *  \return GLuintGLuint Resource ID of obtained image object.
          */
         GLuint get_image(std::string_view imageName) const;
         
-        /**
-         * Get the Image object.
-         * @param imageNameHash Hash code of image name, corresponding to hash code of filename
-         *                      wihtout extension.
-         * @return GLuint Resource ID of obtained image object.
+        /** Get the Image object.
+         *  \param imageNameHash Hash code of image name, corresponding to hash code of filename
+         *                       wihtout extension.
+         *  \return GLuint Resource ID of obtained image object.
          */
         GLuint get_image(int imageNameHash) const;
         
-        /**
-         * Create a blank image object.
-         * @param uniqueImageName Name of newly created blank image.
-         * @return GLuint Resource ID of created image.
+        /** Create a blank image object.
+         *  \param uniqueImageName Name of newly created blank image.
+         *  \return GLuint Resource ID of created image.
          */
         GLuint create_blank_image(std::string_view uniqueImageName);
     private:
