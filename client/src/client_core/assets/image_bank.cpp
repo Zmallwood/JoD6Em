2ca35afd3a8461d5bf35afa00800797d839f20dc
Zmallwood <zmallwood@proton.me>
2024-08-02 06:18:43 +0200

    Save

--- client/src/client_core/assets/image_bank.cpp
+++ client/src/client_core/assets/image_bank.cpp
@@ -1,7 +1,6 @@
-/* image_bank.cpp
- *
- * Copyright 2024 Andreas Åkerberg <zmallwood@proton.me>
- */
+// image_bank.cpp
+
+// Copyright 2024 Andreas Åkerberg <zmallwood@proton.me>
 
 #include "image_bank.h"
 
