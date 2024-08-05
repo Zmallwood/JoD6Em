7c7cbb5c5ff5e95c4a5a95b7162a09447691d1e3
Zmallwood <zmallwood@proton.me>
2024-08-05 15:06:25 +0200

    Save

--- Client/ClientSource/ClientCore/Assets/ImageBank.hpp
+++ Client/ClientSource/ClientCore/Assets/ImageBank.hpp
@@ -7,29 +7,42 @@
 #pragma once
 
 namespace JoD {
-    /// @brief Loads and provides image resources.
+    
+    ///
+    /// Loads and provides image resources.
+    ///
     class ImageBank {
       public:
-        /// @brief Construct a new Image Bank object.
+        ///
+        /// Construct a new Image Bank object.
+        ///
         ImageBank();
         
-        /// @brief Destroy the image bank object.
+        ///
+        /// Destroy the image bank object.
+        ///
         ~ImageBank();
         
-        /// @brief Get the Image object.
+        ///
+        /// Get the Image object.
         /// @param imageName of image, corresponding to filename wihtout extension.
         /// @return GLuint Resource ID of obtained image object.
+        ///
         GLuint GetImage(std::string_view imageName) const;
         
-        /// @brief Get the Image object.
+        ///
+        /// Get the Image object.
         /// @param imageNameHash Hash code of image name, corresponding to hash code of filename
         ///                      wihtout extension.
         /// @return GLuint Resource ID of obtained image object.
+        ///
         GLuint GetImage(int imageNameHash) const;
         
-        /// @brief Create a blank image object.
+        ///
+        /// Create a blank image object.
         /// @param uniqueImageName Name of newly created blank image.
         /// @return GLuint Resource ID of created image.
+        ///
         GLuint CreateBlankImage(std::string_view uniqueImageName);
         
       private:
