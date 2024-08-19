cdbefda38523d26043e69d0e48768f1d2926bd3a
Zmallwood <zmallwood@proton.me>
2024-08-19 16:18:21 +0200

    Save

--- Client/ClientSource/ClientCore/Assets/ImageEntry.hpp
+++ Client/ClientSource/ClientCore/Assets/ImageEntry.hpp
@@ -6,19 +6,9 @@
 #pragma once
 
 namespace JoD {
-
-///
 /// An entry for storing image data in ImageBank.
-///
-/// @details Used for every loaded image file in ImageBank.
-///          It is needed to be able to store the image
-///          dimensions along the the image ID.
-///
 struct ImageEntry {
-    
     GLuint id {0};       ///< OpenGL ID for the stored image as a GL texture.
-    
     Size dimensions;     ///< Dimensions for image obtained during texture initialization.
 };
-
 }
--- Client/ClientSource/ClientCore/DrawInstructions/DrawInstructionsManager.cpp
+++ Client/ClientSource/ClientCore/DrawInstructions/DrawInstructionsManager.cpp
@@ -10,29 +10,20 @@
 #include "TextDrawInstruction.hpp"
 
 namespace JoD {
-
 DrawInstructionsManager::DrawInstructionsManager() {
-    
 // Create a sufficient amount of RIDs for drawing images at game start.
-    for (auto i = 0; i < k_maxNumImageDrawInstructions; i++) {
-        
+    for (auto i = 0; i < k_maxNumImageDrawInstructions; i++)
         m_ridsImages.push_back(_<ImageRenderer>().NewImage());
-    }
-    
 // Create a sufficient amount of RIDs for drawing text at game start.
-    for (auto i = 0; i < k_maxNumTextDrawInstructions; i++) {
-        
+    for (auto i = 0; i < k_maxNumTextDrawInstructions; i++)
         m_ridsText.push_back(_<TextRenderer>().NewString());
-    }
 }
 
 void DrawInstructionsManager::AddImageDrawInstruction(
     int imageNameHash,
     BoxF destination, bool repeatTexture, SizeF textureFillAmount) {
-    
 // Create empty image draw instruction.
     auto newInstr = std::make_unique<ImageDrawInstruction>();
-    
 // Fill with data.
     newInstr->type = DrawInstructionTypes::DrawImage;
     newInstr->rid = m_ridsImages.at(
@@ -42,36 +33,29 @@ void DrawInstructionsManager::AddImageDrawInstruction(
     newInstr->destination = destination;
     newInstr->repeatTexture = repeatTexture;
     newInstr->textureFillAmount = textureFillAmount;
-    
 // Add to inactive buffer.
     m_inactiveBuffer->push_back(std::move(newInstr));
 }
 
-
 void DrawInstructionsManager::AddTextDrawInstruction(
     std::string_view text, PointF position, bool centerAlign) {
-    
 // Create empty text draw instruction.
     auto newInstr = std::make_unique<TextDrawInstruction>();
-    
 // Fill with data.
     newInstr->rid = m_ridsText.at(m_ridCounterText++);
     newInstr->type = DrawInstructionTypes::DrawText;
     newInstr->text = text;
     newInstr->position = position;
     newInstr->centerAligned = centerAlign;
-    
 // Add to inactive buffer.
     m_inactiveBuffer->push_back(std::move(newInstr));
 }
 
 void DrawInstructionsManager::ApplyBuffer() {
-    
 // Swap active and inactive buffers.
     const auto temp = m_activeBuffer;
     m_activeBuffer = m_inactiveBuffer;
     m_inactiveBuffer = temp;
-    
 // Reset state for inactive buffer and rid counters.
     m_inactiveBuffer->clear();
     m_ridCounterImages = 0;
@@ -79,18 +63,13 @@ void DrawInstructionsManager::ApplyBuffer() {
 }
 
 void DrawInstructionsManager::ExecuteInstructions() const {
-    
 // Execute all drawing instructions that have been added.
     for (const auto &instruction : *m_activeBuffer){
-        
         switch (instruction->type) {
-        
         case DrawInstructionTypes::DrawImage: {
-            
 // Cast to image instruction type.
             auto casted =
                 static_cast<ImageDrawInstruction*>(instruction.get());
-            
 // Draw image as specified in the instruction.
             _<ImageRenderer>().DrawImage(
                 casted->rid,
@@ -98,30 +77,24 @@ void DrawInstructionsManager::ExecuteInstructions() const {
                 casted->destination,
                 casted->repeatTexture,
                 casted->textureFillAmount);
-            
             break;
         }
         case DrawInstructionTypes::DrawText: {
-            
 // Cast to text instruction type.
             auto casted =
                 static_cast<TextDrawInstruction*>(instruction.get());
-            
 // Draw string as specified in the instruction.
             _<TextRenderer>().DrawString(
                 casted->rid, casted->text,
                 casted->position,
                 Colors::wheat,
                 casted->centerAligned);
-            
             break;
         }
         case DrawInstructionTypes::None: {
-            
             break;
         }
         }
     }
 }
-
 }
