fd7dbd46c4c9bfbbd255b796f0469365b98c0210
Zmallwood <zmallwood@proton.me>
2024-08-20 21:38:26 +0200

    Save

--- Client/ClientSource/Main.cpp
+++ Client/ClientSource/Main.cpp
@@ -17,5 +17,5 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;  
     }
 // No exceptions occured.  
-    return EXIT_SUCCESS;        
-}   
+    return EXIT_SUCCESS;          
+}    
